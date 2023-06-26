e24ade026a8f35787235b8244481497b594f6ab6
Ujjwal Kashyap <ujjwalkashyap2511@gmail.com>
2023-06-26 14:55:36 +0530

    second commit

---
+++ ProgramForPrintTrangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,m;
+    printf("Enter the numbers of Row(lines)\n");
+    scanf("%d",&m);
+    if(m%2==0) m/=2;
+    n=(m+1)/2;
+    for(int k=0;k<m;k++)
+    {
+        for (int i=n;i>=0;i--) printf(" ");
+        for(int j=0,p=2*k;j<=p;j++) printf("*");
+        printf("\n");
+        n--;
+    }
+    return(0);
+}
